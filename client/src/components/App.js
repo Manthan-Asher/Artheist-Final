eab4124c0f3d691a3bfa8a8ac7e6a7b8ff8b5222
Chahat Bhatia <chahatbhatia90@gmail.com>
2020-06-10 18:52:04 +0530

    [ Chahat | Added Navbar, Carousel & Top Picks Sections for Homepage ]

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React from 'react';
+import './App.css'
+import Carousel from "./Carousel/Carousel";
+import Navbar from "./Navbar/Navbar";
+import TopPicks from "./TopPicks/TopPicks";
 
-export default () => {
-  return <div>Hello World</div>;
-};
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.min.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Carousel />
+      <TopPicks />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Nav, Navbar, Dropdown, Button } from "react-bootstrap";
+import "./Navbar.css";
+
+
+class NavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <Navbar bg="info" expand="lg" >
+          <Navbar.Brand href="#home">artHeist</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home" className="mb4">Feed</Nav.Link>
+              <Nav.Link href="#link">Contest</Nav.Link>
+              <Nav.Link href="#home">Create Contest</Nav.Link>
+              <Nav.Link href="#link">Artists</Nav.Link>
+              <Dropdown>
+                <Dropdown.Toggle variant="none" id="dropdown-basic">
+                  <i class="fa fa-bars" aria-hidden="true"></i>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+
+                  <Dropdown.Item href="#/action-1">About Us</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                   What we do ?
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Creators
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Testimonials
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-4">
+                    Contact Us
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Nav>
+            <Button variant="danger">Sign Up | Login</Button>
+          </Navbar.Collapse>
+        </Navbar>
+      </>
+    );
+  }
+}
+
+export default NavBar;
