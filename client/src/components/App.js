d1949721026a3a2e784bea05ff3a52f5fc8bc7ec
Shreyash <pandeyshreyash2201@gmail.com>
2020-06-11 10:23:51 +0530

    Added hall of fame

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,12 @@
 import React from "react";
+import HallOfFame from "./hall-of-fame/HallOfFame";
+import Testimonials from "./testimonials/Testimonials";
 
 export default () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <HallOfFame />
+      <Testimonials />
+    </div>
+  );
 };
